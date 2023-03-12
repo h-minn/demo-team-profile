df30701e9443db74650c75a8655bd4bb38c37518
Min <45112668+h-minn@users.noreply.github.com>
2023-03-12 15:06:11 +0900

    [update]fat-arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,20 @@ import React from "react";
 import Entry from "./Entry";
 import member from "../member";
 
-function createEntry(member) {
-  return (
-    <Entry
-      key={member.id}
-      img={member.imgURL}
-      name={member.name}
-      message={member.message}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1>
         <span>chikuzenni</span>
       </h1>
-      <dl className="member">{member.map(createEntry)}</dl>
+      <dl className="member">{member.map(member =>
+        <Entry
+          key={member.id}
+          img={member.imgURL}
+          name={member.name}
+          message={member.message}
+        />
+      )}</dl>
     </div>
   );
 }
